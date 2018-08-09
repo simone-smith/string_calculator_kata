d07ed99343751a99de3ead725f3ebdd1842b9e66
Simone <simonecaplin@gmail.com>
2018-08-09 15:54:40 +0100

    Add method returns integer when given one number in a string

--- spec/StringCalculatorSpec.js
+++ spec/StringCalculatorSpec.js
@@ -5,4 +5,8 @@ describe("StringCalculator", function() {
   it("returns 0 for an empty string", function() {
     expect(calculator.add("")).toEqual(0);
   });
+
+  it("when given one number as a string, it returns that number as an integer", function() {
+    expect(calculator.add("1")).toEqual(1);
+  });
 });
